1b8c53294209a9414409f38cb1093955e4036aca
hrawebdev <hrawebdev@gmail.com>
2025-12-29 12:43:53 +0330

    update mobile nav

--- app/[lang]/(app)/(website)/components/header/Header.tsx
+++ app/[lang]/(app)/(website)/components/header/Header.tsx
@@ -1,9 +1,12 @@
 import HeaderProfile from './HeaderProfile';
+import HeaderLogo from '../HeaderLogo';
 
 export default function Header() {
  return (
-  <header className='flex items-center'>
-   <div></div>
+  <header className='bg-background border-b border-input shadow-[0_4px_8px] shadow-black/10 dark:shadow-white/10 z-(--website-header-zindex) h-(--website-header-height)'>
+   <div>
+    <HeaderLogo />
+   </div>
    <div className='grow'></div>
    <div>
     <HeaderProfile />
--- app/[lang]/(app)/(website)/components/mobile-nav/MobileNav.tsx
+++ app/[lang]/(app)/(website)/components/mobile-nav/MobileNav.tsx
@@ -12,17 +12,30 @@ export default function MobileNav() {
   },
  } = useShareDictionary();
  return (
-  <nav className='grid fixed bg-background bottom-0 start-0 end-0 border-t border-input shadow-[0_-2px_8px] shadow-black/10 dark:shadow-white/10 z-(--website-mobile-nav-zindex) h-(--website-mobile-height) lg:hidden grid-cols-3 text-neutral-600 dark:text-neutral-400'>
-   <Button variant='ghost' className='flex-col h-auto w-auto'>
-    <IoHome className='size-6' />
+  <nav className='grid fixed bg-background bottom-0 start-0 end-0 border-t border-input shadow-[0_-4px_8px] shadow-black/10 dark:shadow-white/10 z-(--website-mobile-nav-zindex) h-(--website-mobile-nav-height) lg:hidden grid-cols-3 text-primary/80'>
+   <Button
+    data-active='true'
+    variant='ghost'
+    size={'icon'}
+    className='group flex-col h-auto w-auto data-[active="true"]:text-primary'
+   >
+    <IoHome className='size-6 group-data-[active="true"]:scale-125' />
     <span className='font-medium'>{mobileNavDic.home}</span>
    </Button>
-   <Button variant='ghost' className='flex-col h-auto w-auto'>
-    <FaTicket className='size-6' />
+   <Button
+    variant='ghost'
+    size={'icon'}
+    className='flex-col h-auto w-auto data-[active="true"]:text-primary'
+   >
+    <FaTicket className='size-6 group-data-[active="true"]:scale-125' />
     <span className='font-medium'>{mobileNavDic.newReserve}</span>
    </Button>
-   <Button variant='ghost' className='flex-col h-auto w-auto'>
-    <FaUser className='size-6' />
+   <Button
+    variant='ghost'
+    size={'icon'}
+    className='flex-col h-auto w-auto data-[active="true"]:text-primary'
+   >
+    <FaUser className='size-6 group-data-[active="true"]:scale-125' />
     <span className='font-medium'>{mobileNavDic.profile}</span>
    </Button>
   </nav>
