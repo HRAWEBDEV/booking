1a35f254402510e6f9596cdab1558ea489cc0b23
hrawebdev <hrawebdev@gmail.com>
2025-12-29 13:09:25 +0330

    update header controllers

--- app/[lang]/(app)/(website)/components/header/Header.tsx
+++ app/[lang]/(app)/(website)/components/header/Header.tsx
@@ -1,14 +1,18 @@
 import HeaderProfile from './HeaderProfile';
 import HeaderLogo from '../HeaderLogo';
+import { ModeControllerButton } from '../ModeContoller';
+import { LocaleControllerButton } from '../LocaleController';
 
 export default function Header() {
  return (
-  <header className='bg-background border-b border-input shadow-[0_4px_8px] shadow-black/10 dark:shadow-white/10 z-(--website-header-zindex) h-(--website-header-height)'>
+  <header className='flex items-center p-4 py-2 border-b border-input shadow-[0_4px_8px] shadow-black/10 dark:shadow-white/10 z-(--website-header-zindex) h-(--website-header-height)'>
    <div>
     <HeaderLogo />
    </div>
    <div className='grow'></div>
-   <div>
+   <div className='flex gap-2 lg:gap-4 items-center'>
+    <LocaleControllerButton />
+    <ModeControllerButton />
     <HeaderProfile />
    </div>
   </header>
--- app/[lang]/(app)/(website)/components/header/HeaderProfile.tsx
+++ app/[lang]/(app)/(website)/components/header/HeaderProfile.tsx
@@ -2,5 +2,11 @@ import { Button } from '@/components/ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 export default function HeaderProfile() {
- return <div></div>;
+ return (
+  <Button variant='ghost' size='icon' className='h-auto w-auto'>
+   <Avatar className='size-12'>
+    <AvatarFallback className='size-12'>H</AvatarFallback>
+   </Avatar>
+  </Button>
+ );
 }
