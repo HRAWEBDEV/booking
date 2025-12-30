14fa54074bf65acf9bc3ff5e815e16c56c503704
hrawebdev <hrawebdev@gmail.com>
2025-12-30 12:28:20 +0330

    update footer color

--- app/[lang]/(app)/(website)/components/footer/Footer.tsx
+++ app/[lang]/(app)/(website)/components/footer/Footer.tsx
@@ -1,5 +1,7 @@
 export default function Footer() {
  return (
-  <footer className='min-h-24 bg-neutral-200 dark:bg-orange-800'>footer</footer>
+  <footer className='min-h-24 bg-neutral-200 dark:bg-neutral-800'>
+   footer
+  </footer>
  );
 }
