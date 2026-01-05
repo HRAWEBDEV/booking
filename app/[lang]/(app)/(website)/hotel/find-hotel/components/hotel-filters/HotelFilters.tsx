6eb65df5b777e5bef6e344e88bf517b9508e3cab
poursha98 <ghostworrior8@gmail.com>
2026-01-05 14:01:16 +0330

    make hotel filter container stikcy

--- app/[lang]/(app)/(website)/hotel/find-hotel/components/hotel-filters/HotelFilters.tsx
+++ app/[lang]/(app)/(website)/hotel/find-hotel/components/hotel-filters/HotelFilters.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function HotelFilters() {
  return (
-  <div className='lg:flex flex-col hidden h-[50px] max-w-[200px] w-full bg-black sticky self-start'></div>
+  <div className='lg:flex flex-col hidden h-[50px] max-w-[200px] w-full bg-black sticky self-start top-4'></div>
  );
 }
