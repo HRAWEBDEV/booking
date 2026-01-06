918e4a8d1839296dd854013a056e669a953d7922
hrawebdev <hrawebdev@gmail.com>
2026-01-06 12:28:57 +0330

    update hotel description section

--- app/[lang]/(app)/(website)/hotel/find-hotel/[...hotelID]/components/HotelDatePicker.tsx
+++ app/[lang]/(app)/(website)/hotel/find-hotel/[...hotelID]/components/HotelDatePicker.tsx
@@ -9,7 +9,17 @@ export default function HotelDatePicker({
  dic: PreviewHotelDictionary;
 }) {
  return (
-  <div className='shadow-lg border border-input p-4 rounded-md mb-2'>
+  <form className='shadow-lg border border-input p-4 rounded-md mb-2'>
+   <div className='mb-2 grid grid-cols-2 gap-4 text-xs font-medium text-neutral-600 pb-2 border-b border-input'>
+    <div className='flex flex-col gap-1 items-center p-1 rounded-md bg-neutral-100'>
+     <span>{dic.hotelDatePicker.arrivalTime}</span>
+     <span>12:00</span>
+    </div>
+    <div className='flex flex-col gap-1 items-center p-1 rounded-md bg-neutral-100'>
+     <span>{dic.hotelDatePicker.departureTime}</span>
+     <span>12:00</span>
+    </div>
+   </div>
    <FieldGroup className='gap-5'>
     <Field className='gap-2'>
      <FieldLabel htmlFor='fromDate'>{dic.hotelDatePicker.fromDate}</FieldLabel>
@@ -19,8 +29,10 @@ export default function HotelDatePicker({
      <FieldLabel htmlFor='toDate'>{dic.hotelDatePicker.toDate}</FieldLabel>
      <Input />
     </Field>
-    <Button size='lg'>{dic.hotelDatePicker.search}</Button>
+    <Button type='submit' size='lg'>
+     {dic.hotelDatePicker.search}
+    </Button>
    </FieldGroup>
-  </div>
+  </form>
  );
 }
--- app/[lang]/(app)/(website)/hotel/find-hotel/[...hotelID]/components/hotel-description/HotelDescription.tsx
+++ app/[lang]/(app)/(website)/hotel/find-hotel/[...hotelID]/components/hotel-description/HotelDescription.tsx
@@ -1,11 +1,33 @@
 import { type PreviewHotelDictionary } from '@/internalization/app/dictionaries/website/hotel/preview-hotel/dictionary';
 
-export default function HotelDescription({}: { dic: PreviewHotelDictionary }) {
+export default function HotelDescription({
+ dic,
+}: {
+ dic: PreviewHotelDictionary;
+}) {
  return (
   <section id='hotelDescription' className='mb-4 p-4 shadow-md rounded-md'>
-   <h1 className='text-2xl font-medium mb-2'>
+   <h1 className='text-2xl text-primary font-medium mb-4'>
     <span>هتل کیش</span>
    </h1>
+   <div className='mb-2 grid grid-cols-2 md:grid-cols-4 gap-4 font-medium text-sm text-neutral-600 pb-2 border-b border-input'>
+    <div className='flex text-center flex-col gap-1 items-center p-2 rounded-md bg-neutral-100'>
+     <span>{dic.hotelDatePicker.arrivalTime}</span>
+     <span>12:00</span>
+    </div>
+    <div className='flex text-center flex-col gap-1 items-center p-2 rounded-md bg-neutral-100'>
+     <span>{dic.hotelDatePicker.departureTime}</span>
+     <span>12:00</span>
+    </div>
+    <div className='flex text-center flex-col gap-1 items-center p-2 rounded-md bg-neutral-100'>
+     <span>{dic.hotelDescription.floors}</span>
+     <span>25</span>
+    </div>
+    <div className='flex text-center flex-col gap-1 items-center p-2 rounded-md bg-neutral-100'>
+     <span>{dic.hotelDescription.rooms}</span>
+     <span>12</span>
+    </div>
+   </div>
    <p className='text-sm text-neutral-600 dark:text-neutral-400'>
     لورم ايپسوم متن ساختگي با توليد سادگي نامفهوم از صنعت چاپ، و با استفاده از
     طراحان گرافيک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که
