97e5b389cbea85255c78a586dbc95ab21357f4a0
hrawebdev <hrawebdev@gmail.com>
2026-01-07 08:07:27 +0330

    add facilities title

--- app/[lang]/(app)/(website)/hotel/find-hotel/[...hotelID]/components/hotel-cancel-policies/HotelCancelPolicies.tsx
+++ app/[lang]/(app)/(website)/hotel/find-hotel/[...hotelID]/components/hotel-cancel-policies/HotelCancelPolicies.tsx
@@ -10,7 +10,7 @@ export default function HotelCancelPolicies({
    id='cancelPolicies'
    className='scroll-mt-16 mb-4 p-4 shadow-md rounded-md dark:border dark:border-input'
   >
-   <h3 className='text-lg font-medium text-rose-700 dark:text-rose-400'>
+   <h3 className='text-lg font-medium text-rose-700 dark:text-rose-400 mb-2'>
     {dic.hotelCancelPolicies.title}
    </h3>
    <p className='text-sm text-neutral-600 dark:text-neutral-400'>
--- app/[lang]/(app)/(website)/hotel/find-hotel/[...hotelID]/components/hotel-facilities/HotelFacilities.tsx
+++ app/[lang]/(app)/(website)/hotel/find-hotel/[...hotelID]/components/hotel-facilities/HotelFacilities.tsx
@@ -1,21 +1,30 @@
 import { type PreviewHotelDictionary } from '@/internalization/app/dictionaries/website/hotel/preview-hotel/dictionary';
 import { FaCar } from 'react-icons/fa';
 
-export default function HotelFacilities({}: { dic: PreviewHotelDictionary }) {
+export default function HotelFacilities({
+ dic,
+}: {
+ dic: PreviewHotelDictionary;
+}) {
  return (
   <section
    id='hotelFacilities'
-   className='scroll-mt-16 mb-4 p-4 shadow-md rounded-md grid grid-cols-2 md:grid-cols-4 gap-4 dark:border dark:border-input'
+   className='scroll-mt-16 mb-4 p-4 shadow-md rounded-md dark:border dark:border-input'
   >
-   {Array.from({ length: 12 }, (_, i) => i).map((i) => (
-    <div
-     key={i}
-     className='flex gap-2 items-center text-neutral-600 dark:text-neutral-400'
-    >
-     <FaCar className='size-5' />
-     <span className='text-sm'>پارکینگ</span>
-    </div>
-   ))}
+   <h3 className='text-lg font-medium text-primary mb-3'>
+    {dic.hotelFacilities.title}
+   </h3>
+   <div className='grid grid-cols-2 md:grid-cols-4 gap-4'>
+    {Array.from({ length: 12 }, (_, i) => i).map((i) => (
+     <div
+      key={i}
+      className='flex gap-2 items-center text-neutral-600 dark:text-neutral-400'
+     >
+      <FaCar className='size-5' />
+      <span className='text-sm'>پارکینگ</span>
+     </div>
+    ))}
+   </div>
   </section>
  );
 }
