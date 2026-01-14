9f113b3aef0fef4c8460e24ebe07035e79bf0cc5
hrawebdev <hrawebdev@gmail.com>
2026-01-14 12:49:00 +0330

    add reserve info form and summary loading

--- app/[lang]/(app)/(website)/hotel/reserve/components/reserve-info/ReserveInfoForm.tsx
+++ app/[lang]/(app)/(website)/hotel/reserve/components/reserve-info/ReserveInfoForm.tsx
@@ -1,5 +1,180 @@
+'use client';
 import { type ReserveHotelDictionary } from '@/internalization/app/dictionaries/website/hotel/reserve/dictionary';
+import { FieldLabel, Field } from '@/components/ui/field';
+import { InputGroup, InputGroupInput } from '@/components/ui/input-group';
+import { useReserveConfig } from '../../services/reserve-config/reserveConfigContext';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Label } from '@/components/ui/label';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { useBaseConfig } from '@/services/base-config/baseConfigContext';
+import { Skeleton } from '@/components/ui/skeleton';
 
-export default function ReserveInfoForm({}: { dic: ReserveHotelDictionary }) {
- return <div>form</div>;
+export default function ReserveInfoForm({
+ dic,
+}: {
+ dic: ReserveHotelDictionary;
+}) {
+ const {
+  hotelInfo: { isLoading: hotelInfoIsLoading },
+  rooms: { data, isLoading },
+ } = useReserveConfig();
+ const { localeInfo } = useBaseConfig();
+ return (
+  <div>
+   <form>
+    <section className='p-4 border border-input rounded-md mb-4'>
+     <div>
+      <h3 className='font-medium mb-4 text-neutral-600 dark:text-neutral-400'>
+       {dic.reserveInfo.reserveForm.reservePersonInfo}
+      </h3>
+      <div className='grid gap-4 grid-cols-1 md:grid-cols-2 gap-y-5'>
+       <div className='grid gap-4 grid-cols-2'>
+        <Field className='gap-2'>
+         <FieldLabel htmlFor='firstName'>
+          {dic.reserveInfo.reserveForm.firstName} *
+         </FieldLabel>
+         <InputGroup>
+          <InputGroupInput id='firstName' />
+         </InputGroup>
+        </Field>
+        <Field className='gap-2'>
+         <FieldLabel htmlFor='lastName'>
+          {dic.reserveInfo.reserveForm.lastName} *
+         </FieldLabel>
+         <InputGroup>
+          <InputGroupInput id='lastName' />
+         </InputGroup>
+        </Field>
+       </div>
+       <Field className='gap-2'>
+        <FieldLabel htmlFor='nationalCode'>
+         {dic.reserveInfo.reserveForm.nationalCode} *
+        </FieldLabel>
+        <InputGroup>
+         <InputGroupInput id='nationalCode' />
+        </InputGroup>
+       </Field>
+       <Field className='gap-2'>
+        <FieldLabel htmlFor='email'>
+         {dic.reserveInfo.reserveForm.email}
+        </FieldLabel>
+        <InputGroup>
+         <InputGroupInput id='email' />
+        </InputGroup>
+       </Field>
+       <Field className='gap-2'>
+        <FieldLabel htmlFor='phoneNumber'>
+         {dic.reserveInfo.reserveForm.phoneNumber} *
+        </FieldLabel>
+        <InputGroup>
+         <InputGroupInput id='phoneNumber' />
+        </InputGroup>
+       </Field>
+      </div>
+     </div>
+    </section>
+    <div>
+     <h3 className='font-medium mb-4 text-neutral-600 dark:text-neutral-400'>
+      {dic.reserveInfo.reserveForm.roomsPersonInfo}
+     </h3>
+     {isLoading || hotelInfoIsLoading ? (
+      <>
+       {Array.from({ length: 2 }, (_, i) => i).map((i) => (
+        <Skeleton key={i} className='h-60 mb-4' />
+       ))}
+      </>
+     ) : (
+      data?.map((room, i) => {
+       return (
+        <section key={i} className='p-4 border border-input rounded-md mb-4'>
+         <div className='mb-3'>
+          <h4 className='font-medium mb-2'>
+           {i + 1}) {room.fName}
+          </h4>
+          <p className='font-medium text-neutral-600 dark:text-neutral-400'>
+           {room.accommodationTypePrice.beds}{' '}
+           {dic.reserveInfo.reserveForm.person}
+          </p>
+         </div>
+         <div className='flex flex-wrap gap-5 mb-5'>
+          <Tabs dir={localeInfo.contentDirection} value='male'>
+           <TabsList>
+            <TabsTrigger value='male' className='w-20'>
+             {dic.reserveInfo.reserveForm.male}
+            </TabsTrigger>
+            <TabsTrigger value='female' className='w-20'>
+             {dic.reserveInfo.reserveForm.female}
+            </TabsTrigger>
+           </TabsList>
+          </Tabs>
+          <Tabs dir={localeInfo.contentDirection} value='innerGuest'>
+           <TabsList>
+            <TabsTrigger value='innerGuest'>
+             {dic.reserveInfo.reserveForm.innerGuest}
+            </TabsTrigger>
+            <TabsTrigger value='foreignGuest'>
+             {dic.reserveInfo.reserveForm.foreignGuest}
+            </TabsTrigger>
+           </TabsList>
+          </Tabs>
+         </div>
+         <div className='flex gap-5 flex-wrap mb-8'>
+          <div className='flex gap-3'>
+           <Checkbox
+            id={`saveAsReservePersonInfo${i + 1}`}
+            className='scale-125'
+           />
+           <Label htmlFor={`saveAsReservePersonInfo${i + 1}`}>
+            {dic.reserveInfo.reserveForm.saveAsReservePersonInfo}
+           </Label>
+          </div>
+          <div className='flex gap-3 flex-wrap'>
+           <Checkbox id={`earlyCheckinCharge${i + 1}`} className='scale-125' />
+           <Label htmlFor={`earlyCheckinCharge${i + 1}`}>
+            {dic.reserveInfo.reserveForm.earlyCheckinCharge}
+           </Label>
+          </div>
+          <div className='flex gap-3 flex-wrap'>
+           <Checkbox id={`lateCheckoutCharge${i + 1}`} className='scale-125' />
+           <Label htmlFor={`lateCheckoutCharge${i + 1}`}>
+            {dic.reserveInfo.reserveForm.lateCheckoutCharge}
+           </Label>
+          </div>
+         </div>
+         <div className='grid gap-4 grid-cols-1 md:grid-cols-2 gap-y-5'>
+          <div className='grid gap-4 grid-cols-2'>
+           <Field className='gap-2'>
+            <FieldLabel htmlFor={`firstName${i + 1}`}>
+             {dic.reserveInfo.reserveForm.firstName} *
+            </FieldLabel>
+            <InputGroup>
+             <InputGroupInput id={`firstName${i + 1}`} />
+            </InputGroup>
+           </Field>
+           <Field className='gap-2'>
+            <FieldLabel htmlFor={`lastName${i + 1}`}>
+             {dic.reserveInfo.reserveForm.lastName} *
+            </FieldLabel>
+            <InputGroup>
+             <InputGroupInput id={`lastName${i + 1}`} />
+            </InputGroup>
+           </Field>
+          </div>
+          <Field className='gap-2'>
+           <FieldLabel htmlFor={`nationalCode${i + 1}`}>
+            {dic.reserveInfo.reserveForm.nationalCode} *
+           </FieldLabel>
+           <InputGroup>
+            <InputGroupInput id={`nationalCode${i + 1}`} />
+           </InputGroup>
+          </Field>
+         </div>
+        </section>
+       );
+      })
+     )}
+    </div>
+   </form>
+  </div>
+ );
 }
--- app/[lang]/(app)/(website)/hotel/reserve/components/reserve-info/ReserveInfoSummary.tsx
+++ app/[lang]/(app)/(website)/hotel/reserve/components/reserve-info/ReserveInfoSummary.tsx
@@ -3,6 +3,9 @@ import { type ReserveHotelDictionary } from '@/internalization/app/dictionaries/
 import { useReserveConfig } from '../../services/reserve-config/reserveConfigContext';
 import { useBaseConfig } from '@/services/base-config/baseConfigContext';
 import { useCurrencyFormatter } from '@/hooks/useCurrencyFormatter';
+import { Button } from '@/components/ui/button';
+import { Spinner } from '@/components/ui/spinner';
+import { Skeleton } from '@/components/ui/skeleton';
 
 export default function ReserveInfoSummary({
  dic,
@@ -13,72 +16,112 @@ export default function ReserveInfoSummary({
  const {
   hotelInfo,
   reserveInfo,
-  rooms: { data },
+  rooms: { data, isLoading },
  } = useReserveConfig();
 
  const { locale } = useBaseConfig();
  return (
-  <section className='p-4 rounded-md border border-input'>
-   <div className='mb-3 pb-3 border-b border-input'>
-    <h1 className='text-2xl font-medium'>{hotelInfo.data?.fName}</h1>
-    <p className='text-sm text-neutral-600 dark:text-neutral-400'>
-     {hotelInfo.data?.address}
-    </p>
-   </div>
-   <div className='grid grid-cols-[1fr_max-content_1fr] gap-2 justify-center items-center mb-3 pb-3'>
-    <div className='flex flex-col justify-center text-center gap-1'>
-     <p className='text-primary text-sm font-medium'>
-      {dic.reserveInfo.reserveSummary.arrivalDate}
-     </p>
-     <p className='font-medium text-base'>
-      {new Date(reserveInfo.fromDate).toLocaleDateString(locale, {
-       dateStyle: 'full',
-      })}
-     </p>
-     <p className='text-neutral-600 dark:text-neutral-400'>
-      {hotelInfo.data?.checkin}
-     </p>
-    </div>
-    <div className='flex flex-col text-sm text-neutral-500'>
-     <span>- 4 {dic.reserveInfo.reserveSummary.night} -</span>
-    </div>
-    <div className='flex flex-col justify-center text-center gap-1'>
-     <p className='text-primary text-sm font-medium'>
-      {dic.reserveInfo.reserveSummary.departureDate}
-     </p>
-     <p className='font-medium text-base'>
-      {new Date(reserveInfo.toDate).toLocaleDateString(locale, {
-       dateStyle: 'full',
-      })}
-     </p>
-     <p className='text-neutral-600 dark:text-neutral-400'>
-      {hotelInfo.data?.checkout}
-     </p>
-    </div>
-   </div>
-   <div className='flex flex-col'>
-    <ul className='max-h-24 overflow-auto mb-2'>
-     {data?.map((room, i) => (
-      <li
-       key={i}
-       className='flex flex-wrap gap-2 text-xs text-neutral-600 dark:text-neutral-400'
-      >
-       <span>{room.fName}: </span>
-       <div>
-        <span>
-         {numberFormatter.format(room.accommodationTypePrice.netRoomRate)}
+  <div className='row-start-1 lg:row-start-auto'>
+   {isLoading || hotelInfo.isLoading ? (
+    <Skeleton className='w-full h-96' />
+   ) : (
+    <div className='sticky top-1'>
+     <section className='p-4 rounded-md border border-input mb-2'>
+      <div className='mb-3 pb-3 border-b border-input'>
+       <h1 className='text-xl font-medium'>{hotelInfo.data?.fName}</h1>
+       <p className='text-sm text-neutral-600 dark:text-neutral-400'>
+        {hotelInfo.data?.address}
+       </p>
+      </div>
+      <div className='grid grid-cols-[1fr_max-content_1fr] gap-2 justify-center items-center mb-3 pb-3'>
+       <div className='flex flex-col justify-center text-center gap-1'>
+        <p className='text-primary text-sm font-medium'>
+         {dic.reserveInfo.reserveSummary.arrivalDate}
+        </p>
+        <p className='font-medium'>
+         {new Date(reserveInfo.fromDate).toLocaleDateString(locale, {
+          dateStyle: 'full',
+         })}
+        </p>
+        <p className='text-neutral-600 dark:text-neutral-400'>
+         {hotelInfo.data?.checkin}
+        </p>
+       </div>
+       <div className='flex flex-col text-sm text-neutral-500'>
+        <span>- 4 {dic.reserveInfo.reserveSummary.night} -</span>
+       </div>
+       <div className='flex flex-col justify-center text-center gap-1'>
+        <p className='text-primary text-sm font-medium'>
+         {dic.reserveInfo.reserveSummary.departureDate}
+        </p>
+        <p className='font-medium'>
+         {new Date(reserveInfo.toDate).toLocaleDateString(locale, {
+          dateStyle: 'full',
+         })}
+        </p>
+        <p className='text-neutral-600 dark:text-neutral-400'>
+         {hotelInfo.data?.checkout}
+        </p>
+       </div>
+      </div>
+      <div className='flex flex-col border-t border-input pt-4'>
+       <ul className='h-24 overflow-auto mb-2'>
+        {data?.map((room, i) => (
+         <li
+          key={i}
+          className='flex flex-wrap gap-2 text-sm text-neutral-600 dark:text-neutral-400'
+         >
+          <span>{room.fName}: </span>
+          <div>
+           <span>
+            {numberFormatter.format(room.accommodationTypePrice.netRoomRate)}
+           </span>
+           <span className='text-xs'> ریال</span>
+          </div>
+         </li>
+        ))}
+       </ul>
+       <div className='text-rose-700 dark:text-rose-400 mb-1'>
+        <span>+ {dic.reserveInfo.reserveSummary.TotalPrice}: </span>
+        <span className='text-sm line-through'>
+         {numberFormatter.format(121231231)}
         </span>
         <span className='text-xs'> ریال</span>
        </div>
-      </li>
-     ))}
-    </ul>
-    <div className='font-medium'>
-     <span>{dic.reserveInfo.reserveSummary.totalPrice}: </span>
-     <span className='text-lg'>{numberFormatter.format(121231231)}</span>
-     <span className='text-xs'> ریال</span>
+       <div className='text-secondary mb-3 pb-2 border-b border-input'>
+        <span>- {dic.reserveInfo.reserveSummary.totalDiscount}: </span>
+        <span className='text-base'>{numberFormatter.format(121231231)}</span>
+        <span className='text-xs'> ریال</span>
+       </div>
+       <div className='font-medium text-primary'>
+        <span>{dic.reserveInfo.reserveSummary.discountTotalPrice}: </span>
+        <span className='text-lg'>{numberFormatter.format(121231231)}</span>
+        <span className='text-xs'> ریال</span>
+       </div>
+      </div>
+     </section>
+     <div className='grid gap-4 grid-cols-2'>
+      <Button
+       className='text-base'
+       variant='outline'
+       size='lg'
+       disabled={isLoading || hotelInfo.isLoading}
+      >
+       {(isLoading || hotelInfo.isLoading) && <Spinner />}
+       {dic.reserveInfo.reserveForm.cancel}
+      </Button>
+      <Button
+       className='text-base'
+       variant='secondary'
+       size='lg'
+       disabled={isLoading || hotelInfo.isLoading}
+      >
+       {(isLoading || hotelInfo.isLoading) && <Spinner />}
+       {dic.reserveInfo.reserveForm.payment}
+      </Button>
+     </div>
     </div>
-   </div>
-  </section>
+   )}
+  </div>
  );
 }
