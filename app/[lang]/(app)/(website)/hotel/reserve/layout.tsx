dcc3096f3ec56d64275f045d778509ae61baeff3
hrawebdev <hrawebdev@gmail.com>
2026-01-18 15:01:10 +0330

    move reserve config provider

--- app/[lang]/(app)/(website)/hotel/reserve/layout.tsx
+++ app/[lang]/(app)/(website)/hotel/reserve/layout.tsx
@@ -1,15 +1,5 @@
-import { getReserveHotelDictionary } from '@/internalization/app/dictionaries/website/hotel/reserve/dictionary';
-import { type Locale } from '@/internalization/app/localization';
-import ReserveConfigProvider from './services/reserve-config/ReserveConfigProvider';
-
-export default async function ReserveHotelLayout(
+export default function ReserveHotelLayout(
  props: LayoutProps<'/[lang]/hotel/reserve'>,
 ) {
- const { lang } = await props.params;
- const dic = await getReserveHotelDictionary({
-  locale: lang as Locale,
- });
- return (
-  <ReserveConfigProvider dic={dic}>{props.children}</ReserveConfigProvider>
- );
+ return <>{props.children}</>;
 }
--- app/[lang]/(app)/(website)/hotel/reserve/page.tsx
+++ app/[lang]/(app)/(website)/hotel/reserve/page.tsx
@@ -1,6 +1,7 @@
 import { getReserveHotelDictionary } from '@/internalization/app/dictionaries/website/hotel/reserve/dictionary';
 import { type Locale } from '@/internalization/app/localization';
 import ReserveWrapper from './components/ReserveWrapper';
+import ReserveConfigProvider from './services/reserve-config/ReserveConfigProvider';
 
 export default async function HotelHomePage(
  props: PageProps<'/[lang]/hotel/reserve'>,
@@ -10,5 +11,9 @@ export default async function HotelHomePage(
   locale: lang as Locale,
  });
 
- return <ReserveWrapper dic={dic} />;
+ return (
+  <ReserveConfigProvider dic={dic}>
+   <ReserveWrapper dic={dic} />
+  </ReserveConfigProvider>
+ );
 }
