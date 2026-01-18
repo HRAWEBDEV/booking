a9a14a518efaf85a13523b63ec54b2c129b6393a
hrawebdev <hrawebdev@gmail.com>
2026-01-18 15:48:26 +0330

    add enamd image to footer

--- app/[lang]/(app)/(website)/components/footer/Footer.tsx
+++ app/[lang]/(app)/(website)/components/footer/Footer.tsx
@@ -1,5 +1,26 @@
 export default function Footer() {
  return (
-  <footer className='min-h-24 bg-neutral-200 dark:bg-neutral-800'></footer>
+  <footer className='min-h-24 bg-neutral-200 dark:bg-neutral-800'>
+   <div className='flex justify-end'>
+    <a
+     className='size-28'
+     target='_blank'
+     href={
+      'https://trustseal.enamad.ir/?id=690246&Code=RY1LetjZRJ0stnxZfW88jXpYCEJbLtwj'
+     }
+    >
+     <img
+      referrerPolicy={'origin'}
+      src={
+       'https://trustseal.enamad.ir/logo.aspx?id=690246&Code=RY1LetjZRJ0stnxZfW88jXpYCEJbLtwj'
+      }
+      alt=''
+      style={{ cursor: 'pointer' }}
+      // @ts-expect-error
+      code='RY1LetjZRJ0stnxZfW88jXpYCEJbLtwj'
+     />
+    </a>
+   </div>
+  </footer>
  );
 }
