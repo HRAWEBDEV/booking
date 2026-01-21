f5b5460fe5f9e925fe0bb032090d08d3e9c854bd
hrawebdev <hrawebdev@gmail.com>
2026-01-21 12:35:10 +0330

    update loading heihgt

--- app/[lang]/(app)/(website)/hotel/find-hotel/[hotelID]/loading.tsx
+++ app/[lang]/(app)/(website)/hotel/find-hotel/[hotelID]/loading.tsx
@@ -1,7 +1,7 @@
 import { Spinner } from '@/components/ui/spinner';
 export default function Loading() {
  return (
-  <div className='h-[calc(100svh-var(--website-header-height))] grid place-content-center'>
+  <div className='min-h-[calc(90svh-var(--website-header-height))] grid place-content-center'>
    <Spinner className='size-16 text-primary' />
   </div>
  );
