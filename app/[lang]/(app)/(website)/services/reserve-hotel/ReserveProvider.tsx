d0eb0c010ac28e672ec49112d234e6feafcac3c7
poursha98 <ghostworrior8@gmail.com>
2026-01-27 16:54:06 +0330

    fix useMediaQuery hook, updated and customized drawer

--- app/[lang]/(app)/(website)/services/reserve-hotel/ReserveProvider.tsx
+++ app/[lang]/(app)/(website)/services/reserve-hotel/ReserveProvider.tsx
@@ -14,8 +14,9 @@ import {
  DialogContent,
  DialogHeader,
  DialogTitle,
- DialogDescription,
 } from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 interface ReserveProviderProps {
  children: ReactNode;
@@ -31,19 +32,58 @@ export default function ReserveProvider({ children }: ReserveProviderProps) {
 
    {isDesktop ? (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
-     <DialogContent>
+     <DialogContent className='w-full p-4'>
       <DialogHeader>
-       <DialogTitle>Reserve Hotel</DialogTitle>
-       <DialogDescription>Desktop view description.</DialogDescription>
+       <DialogTitle className='dark:text-gray-300 text-gray-700'>
+        پیگیری رزرو
+       </DialogTitle>
       </DialogHeader>
+      <div className='flex flex-col gap-4'>
+       <Input
+        type='text'
+        placeholder='کد پیگیری رزرو'
+        className='text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 focus-visible:border-primary!'
+       />
+       <Input
+        type='number'
+        placeholder='شماره تماس'
+        className='text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 focus-visible:border-primary!'
+       />
+       <div className='flex items-center gap-4'>
+        <Button className='flex-1' variant='destructive'>
+         انصراف
+        </Button>
+        <Button className='flex-1'>تایید</Button>
+       </div>
+      </div>
      </DialogContent>
     </Dialog>
    ) : (
     <Drawer open={isOpen} onOpenChange={setIsOpen}>
-     <DrawerContent>
-      <DrawerHeader className='text-center'>
-       <DrawerTitle>Reserve Hotel</DrawerTitle>
+     <DrawerContent className='p-4 [&_div.bg-muted]:bg-primary!'>
+      <DrawerHeader className='text-right px-0'>
+       <DrawerTitle className='dark:text-gray-300 text-gray-700'>
+        پیگیری رزرو
+       </DrawerTitle>
       </DrawerHeader>
+      <div className='flex flex-col gap-4'>
+       <Input
+        type='text'
+        placeholder='کد پیگیری رزرو'
+        className='text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 focus-visible:border-primary!'
+       />
+       <Input
+        type='number'
+        placeholder='شماره تماس'
+        className='text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 focus-visible:border-primary!'
+       />
+       <div className='flex items-center gap-4'>
+        <Button className='flex-1' variant='destructive'>
+         انصراف
+        </Button>
+        <Button className='flex-1'>تایید</Button>
+       </div>
+      </div>
      </DrawerContent>
     </Drawer>
    )}
--- services/base-config/hooks/useMediaQuery.ts
+++ services/base-config/hooks/useMediaQuery.ts
@@ -1,6 +1,12 @@
 import { useEffect, useState } from 'react';
 export function useMediaQuery(query: string) {
- const [value, setValue] = useState(false);
+ const [value, setValue] = useState(() => {
+  if (typeof window !== 'undefined') {
+   return window.matchMedia(query).matches;
+  }
+  return false;
+ });
+
  useEffect(() => {
   function onChange(event: MediaQueryListEvent) {
    setValue(event.matches);
