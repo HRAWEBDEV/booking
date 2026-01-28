14c7797599038bebf6e738e97f3642ec7c73f7f1
hrawebdev <hrawebdev@gmail.com>
2026-01-28 07:38:32 +0330

    update failed reserve voucher page

--- app/[lang]/(app)/(website)/hotel/voucher/components/VoucherWrapper.tsx
+++ app/[lang]/(app)/(website)/hotel/voucher/components/VoucherWrapper.tsx
@@ -1,19 +1,10 @@
 import { type ReserveVoucherDictionary } from '@/internalization/app/dictionaries/website/hotel/voucher/dictionary';
+import FailedReserve from './FailedReserve';
 
 export default function VoucherWrapper({
  dic,
 }: {
  dic: ReserveVoucherDictionary;
 }) {
- return (
-  <div className='p-4 my-10'>
-   <div className='w-[min(100%,30rem)] p-4 bg-red-100 border border-red-700 rounded-md mx-auto'>
-    <div>
-     <h2 className='text-center font-medium text-red-700 text-xl'>
-      {dic.voucherInfo.failedReservation}
-     </h2>
-    </div>
-   </div>
-  </div>
- );
+ return <FailedReserve dic={dic} />;
 }
