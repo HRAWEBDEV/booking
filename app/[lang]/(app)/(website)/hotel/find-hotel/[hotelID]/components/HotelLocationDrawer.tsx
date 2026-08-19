f3274b29a75ea63e41d014873a02467beeda5b1c
poursha98 <ghostworrior8@gmail.com>
2026-08-19 13:22:55 +0330

    feat: add hotel location navigation to Neshan and Google Maps

--- app/[lang]/(app)/(website)/hotel/find-hotel/[hotelID]/components/HotelLocationDrawer.tsx
+++ app/[lang]/(app)/(website)/hotel/find-hotel/[hotelID]/components/HotelLocationDrawer.tsx
@@ -11,8 +11,10 @@ import {
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { FaMapMarkedAlt } from 'react-icons/fa';
+import Image from 'next/image';
 import HotelLocation from './HotelLocation';
 import { type HotelInfo } from '../../../services/hotelApiActions';
+import { openNeshanMap, openGoogleMaps } from '../utils/mapActions';
 
 export default function HotelLocationDrawer({
  dic,
@@ -31,14 +33,50 @@ export default function HotelLocationDrawer({
     </DialogTrigger>
     <DialogContent className='h-[95dvh] w-[95dvw] max-w-dvw flex flex-col'>
      <DialogHeader>
-      <DialogTitle className='font-medium text-lg'>
+      <DialogTitle className='font-medium text-base sm:text-lg pe-8 text-start line-clamp-1'>
        {dic.hotelInfo.location} {hotelInfo.fName}
       </DialogTitle>
      </DialogHeader>
      <div className='grow flex flex-col'>
       <HotelLocation dic={dic} hotelInfo={hotelInfo} isDrawer />
      </div>
-     <DialogFooter>
+     <DialogFooter className='flex flex-col sm:flex-col gap-2 w-full'>
+      <div className='grid grid-cols-2 gap-2 w-full'>
+       <Button
+        size='lg'
+        className='w-full px-2 sm:px-4 text-xs sm:text-sm gap-1.5 sm:gap-2'
+        onClick={() => openNeshanMap(hotelInfo.latitude, hotelInfo.longitude)}
+        disabled={!hotelInfo.latitude || !hotelInfo.longitude}
+       >
+        <Image
+         src='/neshan-logo.svg'
+         alt='Neshan'
+         width={24}
+         height={24}
+         className='size-5 shrink-0 object-contain'
+        />
+        <span className='whitespace-nowrap'>
+         {dic.hotelInfo.routingWithNeshan}
+        </span>
+       </Button>
+       <Button
+        size='lg'
+        className='w-full px-2 sm:px-4 text-xs sm:text-sm gap-1.5 sm:gap-2'
+        onClick={() => openGoogleMaps(hotelInfo.latitude, hotelInfo.longitude)}
+        disabled={!hotelInfo.latitude || !hotelInfo.longitude}
+       >
+        <Image
+         src='/google-maps-icon.svg'
+         alt='Google Maps'
+         width={24}
+         height={24}
+         className='size-5 shrink-0 object-contain'
+        />
+        <span className='whitespace-nowrap'>
+         {dic.hotelInfo.routingWithGoogle}
+        </span>
+       </Button>
+      </div>
       <DialogClose asChild>
        <Button size='lg' variant='outline' className='w-full'>
         {dic.hotelInfo.close}
