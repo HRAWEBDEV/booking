5e564ba6ec2710a2477418dc7b5dcd7e32d851e5
HRAWEBDEV <hrawebdev@gmail.com>
2026-08-22 08:38:38 +0330

    update app version

--- services/base-config/baseConfigContext.ts
+++ services/base-config/baseConfigContext.ts
@@ -13,7 +13,7 @@ interface BaseConfig {
  setLocale: (newLocale: Locale) => unknown;
 }
 
-const appVersion = '1.1.8';
+const appVersion = '2.0.0';
 const appBirthDate = new Date(2025, 1, 1);
 const baseConfigContext = createContext<BaseConfig | null>(null);
 
